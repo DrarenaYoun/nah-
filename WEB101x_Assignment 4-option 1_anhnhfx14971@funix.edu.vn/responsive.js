a49c353c6d7ff80a38f92d4902f03ec2b1e182be
minhtuan2000 <minhtuan270820000@gmail.com>
2022-04-10 13:21:36 +0100

    Update responsive.js

--- WEB101x_Assignment 4-option 1_anhnhfx14971@funix.edu.vn/responsive.js
+++ WEB101x_Assignment 4-option 1_anhnhfx14971@funix.edu.vn/responsive.js
@@ -1,8 +1,8 @@
 function menudd() {
-    var dd = document.getElementsByClassName("loai-banh").style.display = "block|none";
-    if (dd.style.display = "block") {
-        dd.style.display = "none"
-    } else if (dd.style.display = "none") {
-        dd.style.display = "block"
+    var dd = document.getElementsByClassName("loai-banh")[0];
+    if (dd.style.display == "block") {
+        dd.style.display = "none";
+    } else {
+        dd.style.display = "block";
     }
 }
