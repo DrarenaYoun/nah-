443b295083cf233f8e1d270b71363e57c3790308
DrarenaYoun <103100368+DrarenaYoun@users.noreply.github.com>
2022-04-10 18:00:57 +0700

    Update responsive.js

--- WEB101x_Assignment 4-option 1_anhnhfx14971@funix.edu.vn/responsive.js
+++ WEB101x_Assignment 4-option 1_anhnhfx14971@funix.edu.vn/responsive.js
@@ -1,8 +1,8 @@
 function menudd() {
-    var dd = document.getElementsByClassName("loai-banh");
-    if (dd.class == "menu") {
-        dd.className += "responsive";
-    } else {
-        dd.className = "menu";
+    var dd = document.getElementsByClassName("loai-banh").style.display = "block|none";
+    if (dd.style.display = "block") {
+        dd.style.display = "none"
+    } else if (dd.style.display = "none") {
+        dd.style.display = "block"
     }
 }
