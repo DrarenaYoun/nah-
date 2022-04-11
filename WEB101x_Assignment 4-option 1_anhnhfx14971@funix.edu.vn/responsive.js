986e90f2009f98c9738ec8cdee48a1c05cc3c8ac
DrarenaYoun <103100368+DrarenaYoun@users.noreply.github.com>
2022-04-11 12:52:09 +0700

    Update responsive.js

--- WEB101x_Assignment 4-option 1_anhnhfx14971@funix.edu.vn/responsive.js
+++ WEB101x_Assignment 4-option 1_anhnhfx14971@funix.edu.vn/responsive.js
@@ -1,5 +1,5 @@
 function menudd() {
-    var dd = document.getElementById("menu")
+    var dd = document.getElementById("menu")[0];
     if (dd.style.display == "block") {
         dd.style.display = "none";
     } else {
