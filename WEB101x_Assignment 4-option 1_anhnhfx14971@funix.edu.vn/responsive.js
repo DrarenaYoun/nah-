7c3c91ec387488ca6466d4760cbd4b556f44582a
minhtuan2000 <minhtuan270820000@gmail.com>
2022-04-17 11:19:36 +0100

    Update menu

--- WEB101x_Assignment 4-option 1_anhnhfx14971@funix.edu.vn/responsive.js
+++ WEB101x_Assignment 4-option 1_anhnhfx14971@funix.edu.vn/responsive.js
@@ -1,8 +1,19 @@
-function menudd() {
-    var dd = document.getElementsByClassName("font")[0];
-    if (dd.style.display == "block") {
-        dd.style.display = "none";
-    } else {
-        dd.style.display = "block";
+window.addEventListener('click', function(e){   
+    // Trong truong hop nguoi dung click chuot
+    if (document.getElementsByClassName('icon')[0].contains(e.target)){
+        // Neu click vao icon
+        var dds = document.getElementsByClassName("font");
+        for (var dd of dds) {
+            dd.style.visibility = "visible";
+            dd.parentElement.parentElement.style.width = "100%";
+        }
+    } 
+    if (!document.getElementById('menu').contains(e.target)) {
+        // Neu click ra ngoai menu
+        var dds = document.getElementsByClassName("font");
+        for (var dd of dds) {
+            dd.style.visibility = "hidden";
+            dd.parentElement.parentElement.style.width = null;
+        }
     }
-}
+});
